6f76f275347fd8266150e47371ff94abbb2f6ded
Troy Foster <troy.foster@scorm.com>
2011-04-25 14:55:18 -0500

    added a missing ; on the sample config page

--- samples/config.php
+++ samples/config.php
@@ -37,7 +37,7 @@ unset($CFG);
 $CFG->wwwroot = '';  // e.g. "http://localhost/PhpLibrary/samples/";
 
 //Rustici Software SCORM Cloud API Key Settings
-$CFG->scormcloudurl = 'http://cloud.scorm.com/EngineWebServices/'
+$CFG->scormcloudurl = 'http://cloud.scorm.com/EngineWebServices/';
 $CFG->scormcloudsecretkey = ''; // e.g. '3nrJQ50o8AOF7qsP0649KfLyXOlfgyxyyt7ecd2U';
 $CFG->scormcloudappid = ''; // e.g. '123WUXJHRT';
 
