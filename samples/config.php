bb21ccce4fc7fdf76fb7846c4474a76d93110cac
Nathan Skirvin <nathan.skirvin@scorm.com>
2014-11-06 12:47:11 -0600

    Add new configuration elements for app manager.

--- samples/config.php
+++ samples/config.php
@@ -37,7 +37,9 @@ $CFG->wwwroot = '';  // e.g. "http://localhost/PhpLibrary/samples/";
 //Rustici Software SCORM Cloud API Key Settings
 $CFG->scormcloudurl = 'http://cloud.scorm.com/EngineWebServices/';
 $CFG->scormcloudsecretkey = ''; // e.g. '3nrJQ50o8AOF7qsP0649KfLyXOlfgyxyyt7ecd2U';
-$CFG->scormcloudappid = ''; // e.g. '123WUXJHRT';
+$CFG->scormcloudappid = ''; // e.g. '321WUXJHRT';
+$CFG->scormcloudmanagersecretkey = ''; // e.g. 'JQ53nr0o8AOF7qsP0649KfLyXOlfgyxyyt7ecd2U';
+$CFG->scormcloudappmanagerid = ''; // e.g. '123WUXJHRT';
 $CFG->scormcloudorigin = ScormEngineUtilities::getCanonicalOriginString('Your Company', 'Your Application', 'Version 2.0');
 
 ?>
